458477da4648197fb495274fb55c1fd6c825c8df
PVLN CHARAN <50245131+PVLNCHRAN@users.noreply.github.com>
2025-07-23 16:32:14 +0530

    Refactor largeValue.js to improve logic for finding the largest number in an array and enhance variable naming for clarity

--- Loops/largeValue.js
+++ Loops/largeValue.js
@@ -1,14 +1,14 @@
 //Write a function that returns the largest number in an array
 const arr = [5, 0, 7, 10, 8, 17, 1, 66];
-
+let highValue = -1;
+//Logic
 const largeValue = (function (arr) {
-  let highValue = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > arr[highValue]) {
-      highValue = i;
+    if (arr[i] > highValue) {
+      highValue = arr[i];
     }
   }
-  return arr[highValue];
+  return highValue;
 })(arr);
-
+//Result
 console.log("Hightest Value is ", largeValue);
