ff89db4aad1db6386f65fda7cfc33f257778339f
PVLN CHARAN <50245131+PVLNCHRAN@users.noreply.github.com>
2025-07-23 15:11:10 +0530

    Refactor searchValue function to accept array as parameter and update usage examples in comments

--- Loops/searchValue.js
+++ Loops/searchValue.js
@@ -1,9 +1,15 @@
 //Write a function that searches for an element in an array and returns the index
 // If the element is not present then just return -1
 
+//searchValue(arr,3) = 3
+//searchValue(arr,61) = 6
+//searchValue(arr,31) = -1
+
 const arr = [2, 1, 4, 3, 55, 43, 61, 59];
 
-const searchValue = (takeValue) => {
+const result = searchValue(arr, 59);
+
+const searchValue = (arr, takeValue) => {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === takeValue) {
       return i;
@@ -12,5 +18,4 @@ const searchValue = (takeValue) => {
   return -1;
 };
 
-const result = searchValue(59);
 console.log("Result is ", result);
