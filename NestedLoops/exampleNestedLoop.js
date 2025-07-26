8781ecb85821c1326c4989b387212232a35f97c5
PVLN CHARAN <50245131+PVLNCHRAN@users.noreply.github.com>
2025-07-26 07:10:56 +0530

    Fix typo in exampleNestedLoop function name and add a second example with detailed console output for better demonstration of nested loops.

--- NestedLoops/exampleNestedLoop.js
+++ NestedLoops/exampleNestedLoop.js
@@ -1,4 +1,5 @@
-function examplaeNestedLoop(expValue) {
+console.log("\nExample 1\n");
+function exampleNestedLoop(expValue) {
   for (let i = 0; i < expValue; i++) {
     for (let j = 0; j < expValue; j++) {
       console.log("i= ", i, " j=", j);
@@ -7,10 +8,25 @@ function examplaeNestedLoop(expValue) {
   }
 }
 
-examplaeNestedLoop(5);
+exampleNestedLoop(5);
 
-/*
+//Example 2
+console.log("\nExample 2\n");
+function exampleNestedLoop1(expValue) {
+  for (let i = 0; i < expValue; i++) {
+    for (let j = 0; j <= i; j++) {
+      console.log("i= ", i, " j=", j);
+    }
+    console.log("----------------");
+  }
+}
 
+exampleNestedLoop1(5);
+
+/**
+ * OutPut 
+ * 
+ * Example 1
 i=  0  j= 0
 i=  0  j= 1
 i=  0  j= 2
@@ -40,5 +56,31 @@ i=  4  j= 1
 i=  4  j= 2
 i=  4  j= 3
 i=  4  j= 4
+----------------
 
-*/
+Example 2
+
+i=  0  j= 0
+----------------
+i=  1  j= 0
+i=  1  j= 1
+----------------
+i=  2  j= 0
+i=  2  j= 1
+i=  2  j= 2
+----------------
+i=  3  j= 0
+i=  3  j= 1
+i=  3  j= 2
+i=  3  j= 3
+----------------
+i=  4  j= 0
+i=  4  j= 1
+i=  4  j= 2
+i=  4  j= 3
+i=  4  j= 4
+----------------
+ * 
+ * 
+ * 
+ */
