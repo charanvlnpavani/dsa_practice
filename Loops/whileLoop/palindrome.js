3bc1d25360d495e87ea94c99d92f9af6229e4558
PVLN CHARAN <50245131+PVLNCHRAN@users.noreply.github.com>
2025-08-04 08:23:40 +0530

    i will update for loop in palindrome

--- Loops/whileLoop/palindrome.js
+++ Loops/whileLoop/palindrome.js
@@ -1,10 +1,9 @@
-const value = 12321;
+const value = -12321;
 const palindrome = (value) => {
   const original = value;
   let revValue = 0;
-  while (value > 0) {
-    let lastDigit = value % 10;
-    value = Math.floor(value / 10);
+  for (let i = value; i > 0; i = Math.floor(i / 10)) {
+    let lastDigit = i % 10;
     revValue = revValue * 10 + lastDigit;
   }
   return original === revValue
